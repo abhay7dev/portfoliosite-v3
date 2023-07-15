5185ec4e3afe9c521e1c9cfa5cd7da219c9eb8c8
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2023-07-15 12:17:35 -0700

    preload audio

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,5 @@
 Array.from(document.querySelectorAll(".pronounciation")).forEach((ipa) => {
 	ipa.addEventListener("click", () => {
-		const audio = new Audio("/media/abhay.mp3");
-		audio.play();
+		document.querySelector(".pronounciation-audio").play();
 	});
 });
