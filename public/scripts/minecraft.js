9bd151042b7a2882fb94ccdc79a943ccbcd0e034
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2023-07-23 17:05:17 -0700

    Add Minecraft Stuff! And prepare for swup

--- public/scripts/minecraft.js
+++ public/scripts/minecraft.js
@@ -1,15 +1,36 @@
-import { SkinViewer } from "./skinview3d.bundle.js";
-
+import { SkinViewer, WalkingAnimation } from "./skinview3d.bundle.js";
 try {
-
     const skin = (await (await fetch("/api/minecraft")).json()).skin;
+    const canvas = document.querySelector(".skin-container");
+    
+    if(window.innerWidth < window.innerHeight) {
+        canvas.width = window.innerWidth * 0.7;
+        canvas.height = window.innerHeight * 0.6;
+    } else {
+        canvas.width = window.innerWidth * 0.2;
+        canvas.height = window.innerHeight * 0.6;
+    }
 
     let skinViewer = new SkinViewer({
-        canvas: document.getElementById("skin_container"),
-        width: 300,
-        height: 400,
-        skin: skin
+        canvas: canvas,
+        width: canvas.width,
+        height: canvas.height,
+        skin: skin, 
     });
+
+    skinViewer.animation = new WalkingAnimation();
+    skinViewer.nameTag = "MisterChief007"
+    skinViewer.zoom = 0.5;
+
+    skinViewer.cameraLight.intensity = 0.0;
+skinViewer.globalLight.intensity = 1.0;
+
+    if(window.innerWidth < window.innerHeight) {
+        skinViewer.zoom = 0.3;
+    } else {
+        skinViewer.zoom = 0.5;
+    }
+
 } catch(err) {
     alert("Failed to initialize skinview3d: " + err);
 }
--- services/minecraft-skin.js
+++ services/minecraft-skin.js
@@ -1,26 +1,32 @@
 export default class MinecraftSkin {
-    constructor(name) {
+    constructor(name, uuid = "") {
         this.name = name;
+        this.uuid = uuid;
     }
 
     async init() {
-        let uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${this.name}`);
-        if(uuid.status != 200) throw new Error("Failed to Initialize Minecraft Skin Object: " + data.errors.message);
-        uuid = (await uuid.json()).id;
-        
-        const skin = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`);
-        if(skin.status != 200) throw new Error("Failed to Initialize Minecraft Skin Object: " + data.errors.message);
-        this.skin = JSON.parse(atob((await skin.json()).properties[0].value)).textures.SKIN.url;
-        
+        if(!this.uuid) this.#fetchuuid();
         return this;
     }
 
     get url() {
         this.#initCheck();
+        this.#fetchskin();
         return this.skin;
     }
 
+    async #fetchuuid() {
+        let uuidData = await fetch(`https://api.mojang.com/users/profiles/minecraft/${this.name}`);
+        if(uuidData.status != 200) throw new Error("Failed to Initialize Minecraft Skin Object: " + data.errors.message);
+        this.uuid = (await uuidData.json()).id;
+    }
+    async #fetchskin() {
+        const skin = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${this.uuid}`);
+        if(skin.status != 200) throw new Error("Failed to Initialize Minecraft Skin Object: " + data.errors.message);
+        this.skin = (JSON.parse(atob((await skin.json()).properties[0].value)).textures.SKIN.url).replace("http:", "https:");
+    }
+
     #initCheck() {
-        if(!this.skin) throw new Error("Minecraft Skin Object not Initialized. Did you remember to run '.init()'?.");
+        if(!this.uuid) throw new Error("Minecraft Skin Object not Initialized. Did you remember to run '.init()'?.");
     }
 }
